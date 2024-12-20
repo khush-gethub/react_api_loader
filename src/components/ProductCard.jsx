4da7f5b371580d0977aeea8a262a75b33fe492f2
khushgit <khushalsonarghare@gmail.com>
2024-12-20 01:41:06 -0800

    loader complete

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from 'react'
 import Card from './Card'
 import axios from 'axios'
+import { BarLoader } from 'react-spinners'
 
 const ProductCard = () => {
   const [Products, setProducts] = useState([])
@@ -9,8 +10,10 @@ const ProductCard = () => {
   const dataSize = 4;
 
   const getProduct = async () => {
+    setLoader(true)
     const data = await axios.get('https://fakestoreapi.com/products?limit=20')
     setProducts(data.data)
+    setLoader(false)
   }
   useEffect(() => {
     getProduct()
@@ -19,40 +22,54 @@ const ProductCard = () => {
   const startIndex = (page - 1) * dataSize
   const perpagedata = Products.slice(startIndex, startIndex + dataSize)
 
-  const hendelp = ()=>{
+  const hendelp = () => {
     if (page > 1) setpage(page - 1)
   }
 
-  const hendeln = ()=>{
-    if (page < Math.ceil(Products.length/dataSize)) setpage(page + 1) 
+  const hendeln = () => {
+    if (page < Math.ceil(Products.length / dataSize)) setpage(page + 1)
   }
+
+  const [loader, setLoader] = useState(false)
+
+
+
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 lg:gap-4">
-        {perpagedata.map((elem, ind) => (
-          <Card key={ind} {...elem} />
-        ))}
-      </div>
+      {loader ? (
+        <div className="flex justify-center items-center h-dvh">
+          <BarLoader color='#3ce2c1'/>
+        </div>
+      ) : (
+        <>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 lg:gap-4">
+            {perpagedata.map((elem, ind) => (
+              <Card key={ind} {...elem} />
+            ))}
+          </div>
 
-      <div className="container w-full flex justify-between my-5">
-        <button
-          type="button"
-          className="btn btn-primary rounded-md btn-sm"
-          disabled={page === 1}
-          onClick={hendelp}
-        >
-          &larr; Previous
-        </button>
+          <div className="container w-full flex justify-between my-5">
+            <button
+              type="button"
+              className="btn btn-primary rounded-md btn-sm"
+              disabled={page === 1}
+              onClick={hendelp}
+            >
+              &larr; Previous
+            </button>
 
-        <button
-          type="button"
-          className="btn btn-primary rounded-md btn-sm"
-          disabled={page >=Math.ceil(Products.length/dataSize)}
-          onClick={hendeln}
-        >
-          Next &rarr;
-        </button>
-      </div>
+            <button
+              type="button"
+              className="btn btn-primary rounded-md btn-sm"
+              disabled={page >= Math.ceil(Products.length / dataSize)}
+              onClick={hendeln}
+            >
+              Next &rarr;
+            </button>
+          </div>
+        </>
+      )}
+
     </>
   )
 }
